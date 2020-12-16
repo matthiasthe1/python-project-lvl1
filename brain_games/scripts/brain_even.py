856221c4b29129d975a2379bc92fb397c4c87c30
Roman Anisimov <r.anisimov.esq@gmail.com>
2020-12-16 17:42:09 +0500

    brain-even added

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import prompt
+import random
+
+def answer_check(answr, number):
+    if number % 2 and answr == 'no':
+        return True
+    elif number % 2 == 0 and answr == 'yes':
+        return True
+    else:
+    	return False
+ 
+        		
+		
+def main():
+    name = prompt.string('May I have your name? ')
+    print('Hello, ' + name + '!')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    number = random.randint(0, 10000)
+    answr = input('Your answer: ')
+    counter = 0
+    while (counter < 3):
+    	print('Question: ', number)
+    	if answer_check(answr, number):
+    	    print('Correct!')
+    	    counter +=1
+    	elif number % 2:
+    	    print( "'" + answr + "' is wrong answer ;(. Correct answer was 'no'\nLet's try again, " + name + "!")
+    	elif number % 2 == 0:
+    	    print( "'" + answr + "' is wrong answer ;(. Correct answer was 'yes'\nLet's try again, " + name + "!" )
+    print('Congradulations, ' + name + '!')
+    	
+    
+
+if __name__ == '__main__':
+    main()
