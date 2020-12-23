ef4722204dfabac0793a7f654b14cafeb9138337
Roman Anisimov <r.anisimov.esq@gmail.com>
2020-12-23 17:17:13 +0500

    brain-even v1.1

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
-from brain_games import cli
 
 
-def engine(question, the_answer):
+def engine(question, the_answer, name):
     print('Question: ', question)
     answr = input('Your answer: ')
     if the_answer == answr:
@@ -10,5 +9,5 @@ def engine(question, the_answer):
         return True
     else:
         print("'" + str(answr) + "' is wrong answer ;(. Correct answer was " + the_answer)
-        print("Let's try again, " + cli.name + "!")
+        print("Let's try again, " + name + "!")
         return False
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 import random
-from brain_games import cli
+import prompt
 from brain_games import engine
 
 
 def eval(number):
     if number % 2:
-        return 'yes'
-    elif number % 2 == 0:
         return 'no'
+    elif number % 2 == 0:
+        return 'yes'
 
 
 def question_generator():
@@ -16,14 +16,15 @@ def question_generator():
 
 
 def main():
-    cli.welcome_user()
+    name = prompt.string('May I have your name? ')
+    print('Hello, ' + name + '!')
     counter = 0
     while (counter < 3):
         question = question_generator()
         the_answer = eval(question)
-        if engine.engine(question, the_answer):
+        if engine.engine(question, the_answer, name):
             counter += 1
-    print('Congradulations, ' + cli.name + '!')
+    print('Congradulations, ' + name + '!')
 
 
 if __name__ == '__main__':
