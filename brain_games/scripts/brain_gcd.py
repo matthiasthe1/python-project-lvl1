003e1d1e270228a13008322f29ce266d41d7ce3c
Roman Anisimov <r.anisimov.esq@gmail.com>
2020-12-24 19:38:15 +0500

    brain-gcd (remade)

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,36 +1,38 @@
 #!/usr/bin/env python
-import prompt
 import random
+import prompt
+from brain_games import engine
 
 
-def checker(number_1, number_2):
-    while number_1 > 0 and number_2 > 0:
-        if number_1 > number_2:
-            number_1 = number_1 % number_2
+def eval(question):
+    a = question[0]
+    b = question[1]
+    while a > 0 and b > 0:
+        if a > b:
+            a = a % b
         else:
-            number_2 = number_2 % number_1
-    if number_1 > number_2:
-        return number_1
+            b = b % a
+    if a > b:
+        return a
     else:
-        return number_2
+        return b
+
+
+def question_generator():
+    return (random.randint(0, 100), random.randint(0, 100))
 
 
 def main():
     name = prompt.string('May I have your name? ')
     print('Hello, ' + name + '!')
-    print('Find the greatest common divisor of given numbers. ')
     counter = 0
     while (counter < 3):
-        number_1 = random.randint(0, 10000)
-        number_2 = random.randint(0, 10000)
-        print("Question: ", number_1, number_2)
-        answr = input('Your answer: ')
-        if answr == str(checker(number_1, number_2)):
-            print('Correct!')
+        question = question_generator()
+        the_answer = eval(question)
+        q = str(question[0]) + ' ' + str(question[1])
+        if engine.engine(q, the_answer, name):
             counter += 1
-        elif answr != checker(number_1, number_2):
-            print("'" + str(answr) + "' is wrong answer ;(. Correct answer was " + str(checker(number_1, number_2)) + "\nLet's try again, " + name + "!")
-    print('Congradulations, ' + name + '!')
+    print('Congratulations, ' + name + '!')
 
 
 if __name__ == '__main__':
