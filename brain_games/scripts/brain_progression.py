d8c678e2c3aca46809caa1d8be5f91fb76506d6a
Roman Anisimov <r.anisimov.esq@gmail.com>
2020-12-24 20:49:43 +0500

    brain-progression (remade)

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,46 +1,38 @@
 #!/usr/bin/env python
-import prompt
 import random
+import prompt
+from brain_games import engine
 
 
-def checker(number_1, number_2, operation):
-    if operation == ' * ':
-        result = number_1 * number_2
-    elif operation == ' + ':
-        result = number_1 + number_2
-    elif operation == ' - ':
-        result = number_1 - number_2
-    return result
+def question_generator():
+    step = random.randint(0, 10)
+    a_0 = random.randint(0, 10)
+    hid_pos = random.randint(0, 9)
+    i = 0
+    a = a_0
+    s = ''
+    while (i < 10):
+        if i == hid_pos:
+            s += '.. '
+            hidden = a
+            a += step
+        else:
+            s += str(a) + ' '
+            a += step
+        i += 1
+    return (s, hidden)
 
 
 def main():
     name = prompt.string('May I have your name? ')
     print('Hello, ' + name + '!')
-    print('What number is missing in the progression?')
     counter = 0
     while (counter < 3):
-        step = random.randint(0, 10)
-        a_0 = random.randint(0, 10)
-        hid_pos = random.randint(0, 9)
-        i = 0
-        a = a_0
-        print('Question: ')
-        while (i < 10):
-            if i == hid_pos:
-                print('..', end=' ')
-                hidden = a
-                a += step
-            else:
-                print(a, end=' ')
-                a += step
-            i += 1
-        answr = input('\nYour answer: ')
-        if answr == str(hidden):
-            print('Correct!')
+        question = question_generator()
+        the_answer = question[1]
+        if engine.engine(question[0], the_answer, name):
             counter += 1
-        elif answr != str(hidden):
-            print("'" + str(answr) + "' is wrong answer ;(. Correct answer was " + str(hidden) + "\nLet's try again, " + name + "!")
-    print('Congradulations, ' + name + '!')
+    print('Congratulations, ' + name + '!')
 
 
 if __name__ == '__main__':
