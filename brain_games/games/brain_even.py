1ba4b7f687b1831df01765852487a9def112480d
Roman Anisimov <r.anisimov.esq@gmail.com>
2021-02-17 21:45:30 +0500

    brain-even rebuild

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python
 import random
-from brain_games import engine
 
 
 NUMBER_OF_ROUNDS = 3
-GAME_MESSANGE = 'Answer "yes" if the number is even, otherwise answer "no".'
+GAME_DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def answer_calculate(number):
+def is_even(number):
     if number % 2:
         return 'no'
     elif number % 2 == 0:
@@ -15,18 +13,6 @@ def answer_calculate(number):
 
 
 def qa_generate():
-    counter = 0
-    question_list = []
-    answer_list = []
-    while counter < NUMBER_OF_ROUNDS:
-        number = random.randint(0, 100)
-        answer = answer_calculate(number)
-        question_list.append(number)
-        answer_list.append(answer)
-        counter += 1
-    return (question_list, answer_list)
-
-
-def game():
-    q_a = qa_generate()
-    engine.start(q_a[0], q_a[1], GAME_MESSANGE)
+    number = random.randint(0, 100)
+    answer = is_even(number)
+    return (str(number), answer)
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
+from brain_games import engine
 from brain_games.games import brain_even
 
 
 def main():
-    brain_even.game()
+    engine.start(brain_even)
 
 
 if __name__ == '__main__':
