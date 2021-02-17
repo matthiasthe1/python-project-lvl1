3c249cf315dc7193439fdbe1e88d05acb10fd87f
Roman Anisimov <r.anisimov.esq@gmail.com>
2021-02-17 22:03:50 +0500

    brain gcd rebuild

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -1,40 +1,17 @@
-#!/usr/bin/env python
 import random
-from brain_games import engine
 
 
 NUMBER_OF_ROUNDS = 3
-GAME_MESSANGE = 'Find the greatest common divisor of given numbers.'
+GAME_DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 
 
-def answer_calculate(number_1, number_2):
-    while number_1 > 0 and number_2 > 0:
-        if number_1 > number_2:
-            number_1 = number_1 % number_2
-        else:
-            number_2 = number_2 % number_1
-    if number_1 > number_2:
-        answer = number_1
-    else:
-        answer = number_2
-    return answer
+def answer_calculate(a, b):
+    return answer_calculate(b, a % b) if b else a
 
 
 def qa_generate():
-    counter = 0
-    question_list = []
-    answer_list = []
-    while counter < NUMBER_OF_ROUNDS:
-        number_1 = random.randint(0, 100)
-        number_2 = random.randint(0, 100)
-        question = str(number_1) + ' ' + str(number_2)
-        answer = answer_calculate(number_1, number_2)
-        question_list.append(question)
-        answer_list.append(answer)
-        counter += 1
-    return (question_list, answer_list)
-
-
-def game():
-    q_a = qa_generate()
-    engine.start(q_a[0], q_a[1], GAME_MESSANGE)
+    number_1 = random.randint(0, 100)
+    number_2 = random.randint(0, 100)
+    question = '{0} {1}'.format(str(number_1), str(number_2))
+    answer = str(answer_calculate(number_1, number_2))
+    return (question, answer)
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
+from brain_games import engine
 from brain_games.games import brain_gcd
 
 
 def main():
-    brain_gcd.game()
+    engine.start(brain_gcd)
 
 
 if __name__ == '__main__':
