1388a75699669a62bfcd6cd375d0a622e113a46c
Roman Anisimov <r.anisimov.esq@gmail.com>
2021-02-23 20:04:22 +0500

    brain-calc operators module added

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,24 +1,24 @@
 import random
+import operator
 
 
 NUMBER_OF_ROUNDS = 3
 GAME_DESCRIPTION = 'What is the result of the expression?'
-OPERATORS = ['*', '+', '-']
+OPERATORS = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+}
 
 
-def calculate(number_1, number_2, operator):
-    if operator == '*':
-        return number_1 * number_2
-    elif operator == '+':
-        return number_1 + number_2
-    elif operator == '-':
-        return number_1 - number_2
+def calculate(number_1, number_2, operation):
+    return str(OPERATORS[operation](number_1, number_2))
 
 
 def generate_round():
     number_1 = random.randint(0, 100)
     number_2 = random.randint(0, number_1)
-    operator = random.choice(OPERATORS)
-    answer = str(calculate(number_1, number_2, operator))
-    question = '{0} {1} {2}'.format(number_1, operator, number_2)
+    operation = random.choice(list(OPERATORS.keys()))
+    answer = calculate(number_1, number_2, operation)
+    question = '{0} {1} {2}'.format(number_1, operation, number_2)
     return (question, answer)
