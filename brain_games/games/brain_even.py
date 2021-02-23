4aa3d52b6d6f38ca6a1212e2b1118293d8206d0f
Roman Anisimov <r.anisimov.esq@gmail.com>
2021-02-23 20:15:26 +0500

    brain-even variables renamed

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -5,11 +5,11 @@ NUMBER_OF_ROUNDS = 3
 GAME_DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def is_even(number):
-    return "yes" if (number % 2 == 0) else "no"
+def is_even(question):
+    return "yes" if (question % 2 == 0) else "no"
 
 
 def generate_round():
-    number = random.randint(0, 100)
-    answer = is_even(number)
-    return (str(number), answer)
+    question = random.randint(0, 100)
+    answer = is_even(question)
+    return (str(question), answer)
