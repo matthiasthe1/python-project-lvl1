403b111ed39c328a369be9740f5272a2b7516eca
Roman Anisimov <r.anisimov.esq@gmail.com>
2021-02-23 20:21:09 +0500

    brain-gcd minor changes

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -5,13 +5,13 @@ NUMBER_OF_ROUNDS = 3
 GAME_DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 
 
-def answer_calculate(a, b):
-    return answer_calculate(b, a % b) if b else a
+def gcd_calculate(a, b):
+    return gcd_calculate(b, a % b) if b else a
 
 
 def generate_round():
     number_1 = random.randint(0, 100)
     number_2 = random.randint(0, 100)
     question = '{0} {1}'.format(str(number_1), str(number_2))
-    answer = str(answer_calculate(number_1, number_2))
+    answer = str(gcd_calculate(number_1, number_2))
     return (question, answer)
