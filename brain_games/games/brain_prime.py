a8e3f1f2d9afb7f7753afc3bf88c201b40e2e143
Roman Anisimov <r.anisimov.esq@gmail.com>
2021-03-30 23:51:10 +0500

    brain_prime function renamed

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -5,7 +5,7 @@ import random
 GAME_DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'   # noqa
 
 
-def is_prime(question):
+def calculate_prime(question):
     i = 2
     while (i <= int(math.sqrt(question))):
         if question % i == 0:
@@ -16,5 +16,5 @@ def is_prime(question):
 
 def generate_round():
     question = random.randint(2, 100)
-    answer = "yes" if is_prime(question) else "no"
+    answer = "yes" if calculate_prime(question) else "no"
     return (str(question), answer)
