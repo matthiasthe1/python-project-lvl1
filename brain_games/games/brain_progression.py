ba34f333eab5cd774acd522b2d239374751128c7
Roman Anisimov <r.anisimov.esq@gmail.com>
2021-03-31 00:23:36 +0500

    brain_progression minor changes

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -9,17 +9,17 @@ def create_progression(start, step, length):
     return [start + step * x for x in range(0, length)]
 
 
-def get_question(progression, hidden_index):
-    progression = progression[:hidden_index - 1] + [MARKER] + progression[hidden_index:] # noqa
+def get_question(progression, hidden_element_index):
+    progression = progression[:hidden_element_index] + [MARKER] + progression[hidden_element_index + 1:] # noqa
     return (' '.join(map(str, progression)))
 
 
 def generate_round():
     step = random.randint(0, 10)
     start = random.randint(0, 10)
-    hidden_index = random.randint(0, 9)
+    hidden_element_index = random.randint(0, 9)
     length = LENGTH
     progression = create_progression(start, step, length)
-    answer = str(progression[hidden_index])
-    question = get_question(progression, hidden_index)
+    answer = str(progression[hidden_element_index])
+    question = get_question(progression, hidden_element_index)
     return (question, answer)
