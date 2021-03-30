39339c426937099289c39617e0c16c5fe022ebbf
Roman Anisimov <r.anisimov.esq@gmail.com>
2021-03-30 23:35:48 +0500

    map in calc renamed

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -3,7 +3,7 @@ import operator
 
 
 GAME_DESCRIPTION = 'What is the result of the expression?'
-map_symbols_to_operators = {
+map_operator_to_operation = {
     '+': operator.add,
     '-': operator.sub,
     '*': operator.mul,
@@ -11,13 +11,13 @@ map_symbols_to_operators = {
 
 
 def get_answer(number_1, number_2, random_operator):
-    return str(map_symbols_to_operators[random_operator](number_1, number_2))
+    return str(map_operator_to_operation[random_operator](number_1, number_2))
 
 
 def generate_round():
     number_1 = random.randint(0, 100)
     number_2 = random.randint(0, number_1)
-    random_operator = random.choice(list(map_symbols_to_operators.keys()))
+    random_operator = random.choice(list(map_operator_to_operation.keys()))
     answer = get_answer(number_1, number_2, random_operator)
     question = '{0} {1} {2}'.format(number_1, random_operator, number_2)
     return (question, answer)
