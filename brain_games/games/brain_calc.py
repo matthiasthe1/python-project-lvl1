e627baed04ea4f67fc70f50ffab98857e0a0af1e
Roman Anisimov <r.anisimov.esq@gmail.com>
2021-04-02 16:08:48 +0500

    minor changes

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -10,7 +10,7 @@ map_operator_to_operation = {
 }
 
 
-def calculate_answer(number_1, number_2, chosen_operator):
+def calculate(number_1, number_2, chosen_operator):
     return map_operator_to_operation[chosen_operator](number_1, number_2)
 
 
@@ -18,6 +18,6 @@ def generate_round():
     number_1 = random.randint(0, 100)
     number_2 = random.randint(0, number_1)
     chosen_operator = random.choice(list(map_operator_to_operation.keys()))
-    answer = str(calculate_answer(number_1, number_2, chosen_operator))
+    answer = str(calculate(number_1, number_2, chosen_operator))
     question = '{0} {1} {2}'.format(number_1, chosen_operator, number_2)
     return (question, answer)
