8aa2ed51041219d3c4aaf2b9965c060b3fb14f9f
Roman Anisimov <r.anisimov.esq@gmail.com>
2021-04-02 20:52:06 +0500

    progression minor changes

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -1,14 +1,15 @@
 import random
 
 LENGTH = 10
+MARKER = '..'
 DESCRIPTION = 'What number is missing in the progression?'
 
 
-def create_progression(start, step, LENGTH):
-    return [start + step * i for i in range(0, LENGTH)]
+def create_progression(start, step, length):
+    return [start + step * i for i in range(0, length)]
 
 
-def get_question(progression, hidden_element_index, marker='..'):
+def get_question(progression, hidden_element_index, marker=MARKER):
     copied_progression = progression[:]
     copied_progression[hidden_element_index] = marker
     return (' '.join(map(str, copied_progression)))
