de919fad26c9f58c7bf71fcdf5b1e9d8236df408
Roman Anisimov <r.anisimov.esq@gmail.com>
2021-04-02 15:31:48 +0500

    progression, calc minor changes

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -10,14 +10,14 @@ map_operator_to_operation = {
 }
 
 
-def calculate_answer(number_1, number_2, random_operator):
-    return map_operator_to_operation[random_operator](number_1, number_2)
+def calculate_answer(number_1, number_2, chosen_operator):
+    return map_operator_to_operation[chosen_operator](number_1, number_2)
 
 
 def generate_round():
     number_1 = random.randint(0, 100)
     number_2 = random.randint(0, number_1)
-    random_operator = random.choice(list(map_operator_to_operation.keys()))
-    answer = str(calculate_answer(number_1, number_2, random_operator))
-    question = '{0} {1} {2}'.format(number_1, random_operator, number_2)
+    chosen_operator = random.choice(list(map_operator_to_operation.keys()))
+    answer = str(calculate_answer(number_1, number_2, chosen_operator))
+    question = '{0} {1} {2}'.format(number_1, chosen_operator, number_2)
     return (question, answer)
--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -5,8 +5,8 @@ DESCRIPTION = 'What number is missing in the progression?'
 MARKER = '..'
 
 
-def create_progression(start, step, length):
-    return [start + step * i for i in range(0, length)]
+def create_progression(start, step, LENGTH):
+    return [start + step * i for i in range(0, LENGTH)]
 
 
 def get_question(progression, hidden_element_index):
@@ -18,8 +18,7 @@ def generate_round():
     step = random.randint(0, 10)
     start = random.randint(0, 10)
     hidden_element_index = random.randint(0, 9)
-    length = LENGTH
-    progression = create_progression(start, step, length)
+    progression = create_progression(start, step, LENGTH)
     answer = str(progression[hidden_element_index])
     question = get_question(progression, hidden_element_index)
     return (question, answer)
